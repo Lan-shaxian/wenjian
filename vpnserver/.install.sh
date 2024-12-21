66d0288489d380e2a0ee4d2dcce30e14e38c0863
Lan-shaxian <631826513@qq.com>
2024-12-22 01:29:14 +0800

    Add files via upload

---
+++ vpnserver/.install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "--------------------------------------------------------------------"
+echo
+echo "SoftEther VPN Server (Ver 4.42, Build 9798, Intel x64 / AMD64) for Linux Build Utility"
+echo "Copyright (c) SoftEther Project at University of Tsukuba, Japan. All Rights Reserved."
+echo
+echo "--------------------------------------------------------------------"
+echo
+echo
+
+cat ReadMeFirst_License.txt
+
+echo
+echo "--------------------------------------------------------------------"
+echo
+echo
+
+make main
+
+exit 0
+
